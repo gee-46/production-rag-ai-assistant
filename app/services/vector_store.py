32e677c153de6d647c3af4e74e8df6eb769e5abb
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-05-14 23:42:35 +0530

    Update vector_store.py

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,17 +1,4 @@
-import faiss
-import numpy as np
+# -----------------------------
+# Create / Load Vector Store
+# -----------------------------
 
-class VectorStore:
-    def __init__(self, dim):
-        self.index = faiss.IndexFlatL2(dim)
-        self.texts = []
-
-    def add(self, embeddings, texts):
-        self.index.add(np.array(embeddings).astype('float32'))
-        self.texts.extend(texts)
-
-    def search(self, query_embedding, k=3):
-        D, I = self.index.search(
-            np.array([query_embedding]).astype('float32'), k
-        )
-        return [self.texts[i] for i in I[0]]
